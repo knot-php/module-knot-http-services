8e1e870aee59de2e614441533609e1a6d404b028
Katsuki Shuto <stk2k@sazysoft.com>
2021-06-07 13:59:19 +0900

    Signed-off-by: Katsuki Shuto <stk2k@sazysoft.com>

--- test/classes/TestFileSystem.php
+++ test/classes/TestFileSystem.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace knotphp\module\knothttpservice\test\classes;
 
-use knotlib\kernel\fileSystem\AbstractFileSystem;
-use knotlib\kernel\fileSystem\FileSystemInterface;
+use knotlib\kernel\filesystem\AbstractFileSystem;
+use knotlib\kernel\filesystem\FileSystemInterface;
 
 final class TestFileSystem extends AbstractFileSystem implements FileSystemInterface
 {
